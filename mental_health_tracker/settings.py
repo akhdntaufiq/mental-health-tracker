11474912c0ccc30d7a7d19c69f050d51014d0160
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2024-09-14 18:49:15 +0700

    update pws project link

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://akhdan-taufiq-mentalhealthtracker10.pbp.cs.ui.ac.id", "https://akhdan-taufiq-mentalhealthtracker10.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://akhdan-taufiq-mentalhealthtracker.pbp.cs.ui.ac.id", "https://akhdan-taufiq-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 
