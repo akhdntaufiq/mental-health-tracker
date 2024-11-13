92d1f7c34a8a1a856a8ad0361aebba0cbc838ba2
akhdntaufiq <akhdanakhdantaufiq812@gmail.com>
2024-11-13 16:13:53 +0700

    TUTORIAL 8

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.core import serializers
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
@@ -12,6 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
+import json
 
 def create_mood_entry(request):
     form = MoodEntryForm(request.POST or None)
@@ -129,3 +130,21 @@ def add_mood_entry_ajax(request):
     new_mood.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_mood_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_mood = MoodEntry.objects.create(
+            user=request.user,
+            mood=data["mood"],
+            mood_intensity=int(data["mood_intensity"]),
+            feelings=data["feelings"]
+        )
+
+        new_mood.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
